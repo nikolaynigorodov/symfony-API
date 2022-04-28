ab3efa430a1ab4c35ef5935edd82b4c89e3f5d0d
Нігородов Микола <nikolaynigorodov@gmail.com>
2022-04-28 14:52:23 +0300

    NelmioApiDoc settings BookController action booksByCategory

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Exception\BookCategoryNotFoundException;
 use App\Service\BookService;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\BookListResponse;
 
 class BookController extends AbstractController
 {
@@ -15,7 +18,14 @@ class BookController extends AbstractController
     {
     }
 
-    #[Route('/api/v1/category/{id}/books')]
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns books inside a category",
+     *     @Model(type=BookListResponse::class)
+     * )
+     */
+    #[Route(path: '/api/v1/category/{id}/books', methods: ['GET'])]
     public function booksByCategory(int $id): Response
     {
         try {
