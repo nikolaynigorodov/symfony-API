fd0948b6758d9f07aecc522ec8b59df5ad075ba9
Нігородов Микола <nikolaynigorodov@gmail.com>
2022-05-01 16:58:58 +0300

    refactroring Tests and more settings

--- migrations/Version20220501135709.php
+++ migrations/Version20220501135709.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220428093618 extends AbstractMigration
+final class Version20220501135709 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- tests/Controller/BookCategoryControllerTest.php
+++ tests/Controller/BookCategoryControllerTest.php
@@ -2,20 +2,37 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\BookCategory;
+use App\Tests\AbstractTestController;
 
-class BookCategoryControllerTest extends WebTestCase
+class BookCategoryControllerTest extends AbstractTestController
 {
     public function testCategories(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/api/v1/book/categories');
-        $requestContent = $client->getResponse()->getContent();
+        $this->em->persist((new BookCategory())->setTitle('Devices')->setSlug('devices'));
+        $this->em->flush();
+
+        $this->client->request('GET', '/api/v1/book/categories');
+        $responseContent = json_decode($this->client->getResponse()->getContent(), true);
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonStringEqualsJsonFile(
-            __DIR__.'/responses/BookCategoryControllerTest.testCategories.json',
-            $requestContent
-        );
+        $this->assertJsonDocumentMatchesSchema($responseContent, [
+            'type' => 'object',
+            'required' => ['items'],
+            'properties' => [
+                'items' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'object',
+                        'required' => ['id', 'title', 'slug'],
+                        'properties' => [
+                            'title' => ['type' => 'string'],
+                            'slug' => ['type' => 'string'],
+                            'id' => ['type' => 'integer'],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
     }
 }
