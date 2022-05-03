7d12f4cfab76ca3f0cd2751e6edca09e40df4a58
Нігородов Микола <nikolaynigorodov@gmail.com>
2022-05-03 14:18:15 +0300

    Exception

--- src/Exception/BookCategoryNotFoundException.php
+++ src/Exception/BookCategoryNotFoundException.php
@@ -3,12 +3,11 @@
 namespace App\Exception;
 
 use RuntimeException;
-use Symfony\Component\HttpFoundation\Response;
 
 class BookCategoryNotFoundException extends RuntimeException
 {
     public function __construct()
     {
-        parent::__construct('Book category not Foud', Response::HTTP_NOT_FOUND);
+        parent::__construct('Book category not Found');
     }
 }
